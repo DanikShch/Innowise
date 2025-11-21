5763fa1fccf304951680061727598af65c3366c0
Danik <bravedanik@gmail.com>
2025-11-21 12:20:46 +0300

    order service entities

---
+++ OrderService/src/main/java/com/innowise/orderservice/model/Order.java
@@ -0,0 +1,19 @@
+package com.innowise.orderservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+}
---
+++ OrderService/src/main/java/com/innowise/orderservice/model/OrderStatus.java
@@ -0,0 +1,4 @@
+package com.innowise.orderservice.model;
+
+public enum OrderStatus {
+}
